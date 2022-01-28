35455767eede6bc595c80e7b893b713fccfba11d
lalekov <lalekov@htl-steyr.ac.at>
2022-01-28 09:39:37 +0100

    Projekt erstellt Spielfeld.java Klasse bearbeitet Methoden geschrieben (noch nicht implementiert...)

--- src/main/Model/Spielfeld.java
+++ src/main/Model/Spielfeld.java
@@ -1,5 +1,25 @@
 package main.Model;
 
+//Spielfeld Klasse
 public class Spielfeld {
+private Spieler spielfeld[][];
 
+    public Spielfeld(Spieler[][] spielfeld) {
+        this.spielfeld = spielfeld;
+    }
+
+    public void einwerfen(int einw, Spieler spieler){
+
+}
+public void checkifboardfull(){
+
+}
+boolean checkwin(Spieler spieler){
+
+    return false;
+}
+boolean checkcolfull(int col){
+
+    return false;
+}
 }
