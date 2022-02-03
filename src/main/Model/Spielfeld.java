12bfb17ef17d59ee6254b6962dfa4bfa8a91e37e
Semir Medzikovic <smedziko@htl-steyr.ac.at>
2022-02-03 15:00:33 +0100

    Spielfeld.java Kommentare

--- src/main/Model/Spielfeld.java
+++ src/main/Model/Spielfeld.java
@@ -2,11 +2,24 @@ package main.Model;
 
 //Spielfeld Klasse
 public class Spielfeld {
+    //Spielfeld erstellen
     int[][] spielfeld = new int[6][7];
+    //Spielsteine erstellen
     Spielstein[][] spielsteine = new Spielstein[6][7];
+    //Zeile des Spielfeldes
     private int zeile;
+    //Spalte des Spielfeldes
     private int spalte;
 
+    /*
+    Werfen-Methode
+    Folgende Ausgaben sind möglich:
+    -1 : Fehlwurf
+     0 : Wurf OK
+     1 : Spieler 1 gewonnen
+     2 : Spieler 2 gewonnen
+     Unentschieden: wenn alle Spielsteine verbraucht sind, wird das Spiel als Unentschieden gewertet
+     */
     public int werfen(int spalte, int zeile, int spielsteine[][]) {
         int x = spielfeld.length - 1;
         boolean fortsetzen = true;
