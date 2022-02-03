ad40e9fc715d98b0243aa6d0cdc7c5eedc453dc4
lalekov <lalekov@htl-steyr.ac.at>
2022-02-03 14:35:40 +0100

    Spielfeld class

--- src/main/Model/Spielfeld.java
+++ src/main/Model/Spielfeld.java
@@ -2,6 +2,11 @@ package main.Model;
 
 //Spielfeld Klasse
 public class Spielfeld {
-    static int[][] SpielFeld = new int[6][7];
+    Spielstein[][] spielsteine = new Spielstein[6][7];
+    private int zeile;
+    private int spalte;
 
+    public int werfen(int spalte,int zeile, int spielsteine[][]){
+
+    }
 }
--- src/main/Model/Spielstein.java
+++ src/main/Model/Spielstein.java
@@ -1,4 +1,21 @@
 package main.Model;
 
+import java.awt.*;
+
 public class Spielstein {
+    private char zeichen;
+    private Color color;
+
+    public Spielstein(char zeichen, Color color) {
+        this.zeichen = zeichen;
+        this.color=color;
+    }
+
+    public char getZeichen() {
+        return zeichen;
+    }
+
+    public Color getColor() {
+        return color;
+    }
 }
