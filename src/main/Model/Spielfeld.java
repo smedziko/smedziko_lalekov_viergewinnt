fd8378240dd85e966a3d4d07e1d62efe74beecf4
lalekov <lalekov@htl-steyr.ac.at>
2022-02-11 09:31:33 +0100

    .

--- src/main/Model/Spielfeld.java
+++ src/main/Model/Spielfeld.java
@@ -1,43 +1,103 @@
 package main.Model;
 
+import java.util.Arrays;
+
 //Spielfeld Klasse
 public class Spielfeld {
-    //Spielfeld erstellen
-    int[][] spielfeld = new int[6][7];
-    //Spielsteine erstellen
-    Spielstein[][] spielsteine = new Spielstein[6][7];
-    //Zeile des Spielfeldes
-    private int zeile;
-    //Spalte des Spielfeldes
-    private int spalte;
 
-    /*
-    Werfen-Methode
-    Folgende Ausgaben sind möglich:
-    -1 : Fehlwurf
-     0 : Wurf OK
-     1 : Spieler 1 gewonnen
-     2 : Spieler 2 gewonnen
-     Unentschieden: wenn alle Spielsteine verbraucht sind, wird das Spiel als Unentschieden gewertet
-     */
-    public int werfen(int spalte, int zeile, int spielsteine[][]) {
-        int x = spielfeld.length - 1;
-        boolean fortsetzen = true;
-        while (x>=0 && fortsetzen==true)
-        {
-            if(spielfeld[x][spalte] == 0 )
-            {
-                spielfeld[x][spalte] = ;
-                fortsetzen = false;
-            }
+    private int[][] feld = null;
 
-            if(x<0){
-                System.out.println("Stein konnte nicht gesetzt werden");
-            }
+    //Spielfeld Konstruktor
+    public Spielfeld(int x, int y) {
+        feld = new int[x][y];
+
+        for (int[] ints : feld) {
+            Arrays.fill(ints, 0);
+
         }
     }
+    //Setzen von Steine im Spielfeld
+    public void Setzen(int spielerid, int spalte) {
 
-    public int gewinnbedingung(int spielsteine[][]) {
+        int x = feld.length - 1;
+
+        boolean weitergehts = true;
+
+        while (x >= 0 && weitergehts) {
+
+            if (feld[x][spalte] == 0) {
+                feld[x][spalte] = spielerid;
+                weitergehts = false;
+            }
+            x--;
+        }
+        //Überprüfung falls falsch eingesetzt
+        if (x < 0) {
+            System.out.println("Der Stein konnte nicht gesetzt werden! Bitte versuchen Sie nochmal.");
+        }
+    }
+    //Ausgabe vom Spielfeld
+    public void Ausgabe() {
+
+        for (int[] ints : feld) {
+            for (int anInt : ints) {
+
+                System.out.print("| "+anInt+" |");
+
+            }
+            System.out.println();
+        }
+
+    }
+
+    //Unentschieden überprüfen (wenn alle Steine gesetzt sind)
+    public boolean drawVoll() {
+
+        return AnzahlGesSteine() == feld.length * feld[0].length;
+    }
+    //Anzahl der gesetzten Steine
+    public int AnzahlGesSteine() {
+        int countSteine = 0;
+        for (int[] ints : feld) {
+            for (int k=0; k<feld[k].length; k++) {
+                if (ints[k] != 0) {
+                    countSteine++;
+                }
+            }
+        }
+
+        return countSteine;
+    }
+    //Testen ob einsetzen in der Reihe möglich ist
+    public boolean testeReihe(int spielerid) {
+        boolean ret = false;
+
+        if (AnzahlGesSteine() >= 7) {
+            for (int i = 0; i < feld.length; i++) {
+                for (int j = 0; j < feld.length; j++) {
+                    if (testHoriz(i, j, spielerid) || testVert(i, j, spielerid) || testDiag(i, j, spielerid)) {
+                        ret = true;
+                    }
+
+                }
+            }
+
+        } else {
+            return ret;
+        }
+        return ret;
+    }
+    //Testen ob einsetzen horizontal möglich ist
+    public boolean testHoriz(int x, int y, int Spieler) {
+        boolean ret = false;
+
+
+        if (y + 3 < feld[x].length) {
+            if (feld[x][y] == Spieler && feld[x][y+1] == Spieler && feld[x][y+2] == Spieler && feld[x][y+3] == Spieler) {
+                ret = true;
+            }
+        }
+        return ret;
 
     }
 }
