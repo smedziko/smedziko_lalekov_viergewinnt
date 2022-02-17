ef04acb4e78ee662173863d263477013563fa22d
lalekov <lalekov@htl-steyr.ac.at>
2022-02-17 12:31:35 +0100

    .

--- src/main/Model/Spielfeld.java
+++ src/main/Model/Spielfeld.java
@@ -16,6 +16,7 @@ public class Spielfeld {
 
         }
     }
+
     //Setzen von Steine im Spielfeld
     public void Setzen(int spielerid, int spalte) {
 
@@ -36,13 +37,14 @@ public class Spielfeld {
             System.out.println("Der Stein konnte nicht gesetzt werden! Bitte versuchen Sie nochmal.");
         }
     }
+
     //Ausgabe vom Spielfeld
     public void Ausgabe() {
 
         for (int[] ints : feld) {
             for (int anInt : ints) {
 
-                System.out.print("| "+anInt+" |");
+                System.out.print("| " + anInt + " |");
 
             }
             System.out.println();
@@ -55,11 +57,12 @@ public class Spielfeld {
 
         return AnzahlGesSteine() == feld.length * feld[0].length;
     }
+
     //Anzahl der gesetzten Steine
     public int AnzahlGesSteine() {
         int countSteine = 0;
         for (int[] ints : feld) {
-            for (int k=0; k<feld[k].length; k++) {
+            for (int k = 0; k < feld[k].length; k++) {
                 if (ints[k] != 0) {
                     countSteine++;
                 }
@@ -68,6 +71,7 @@ public class Spielfeld {
 
         return countSteine;
     }
+
     //Testen ob einsetzen in der Reihe möglich ist
     public boolean testeReihe(int spielerid) {
         boolean ret = false;
@@ -86,6 +90,34 @@ public class Spielfeld {
             return ret;
         }
         return ret;
+    }
+
+    //Testen ob einsetzen vertikal möglich ist
+    public boolean testVert(int x, int y, int Spieler) {
+        boolean ret = false;
+        if (x + 3 < feld.length) {
+            if (feld[x][y] == Spieler && feld[x + 1][y] == Spieler && feld[x + 2][y] == Spieler && feld[x + 3][y] == Spieler) {
+                ret = true;
+            }
+        }
+        return ret;
+    }
+
+    //Testen ob einsetzen diagonal möglich ist
+    public boolean testDiag(int x, int y, int Spieler) {
+        boolean ret = false;
+        if (x + 3 < feld.length && y + 3 < feld.length) {
+            if (feld[x][y] == Spieler && feld[x + 1][y + 1] == Spieler && feld[x + 2][y + 2] == Spieler && feld[x + 3][y + 3] == Spieler) {
+                ret = true;
+            }
+        }
+
+        if (x - 3 >= 0 && y + 3 < feld.length) {
+            if (feld[x][y] == Spieler && feld[x - 1][y + 1] == Spieler && feld[x - 2][y + 2] == Spieler && feld[x - 3][y + 3] == Spieler) {
+                ret = true;
+            }
+        }
+        return ret;
 
     }
 
@@ -95,37 +127,12 @@ public class Spielfeld {
 
 
         if (y + 3 < feld[x].length) {
-            if (feld[x][y] == Spieler && feld[x][y+1] == Spieler && feld[x][y+2] == Spieler && feld[x][y+3] == Spieler) {
+            if (feld[x][y] == Spieler && feld[x][y + 1] == Spieler && feld[x][y + 2] == Spieler && feld[x][y + 3] == Spieler) {
                 ret = true;
             }
         }
         return ret;
 
     }
-    //Testen ob einsetzen vertikal möglich ist
-    public boolean testVert(int x, int y, int Spieler) {
-        boolean ret = false;
-        if (x + 3 < feld.length) {
-            if (feld[x][y] == Spieler && feld[x+1][y] == Spieler && feld[x+2][y] == Spieler && feld[x+3][y] == Spieler) {
-                ret = true;
-            }
-        }
-        return ret;
-    }
-    //Testen ob einsetzen diagonal möglich ist
-    public boolean testDiag(int x, int y, int Spieler) {
-        boolean ret = false;
-        if (x + 3 < feld.length && y + 3 < feld.length) {
-            if (feld[x][y] == Spieler && feld[x+1][y+1] == Spieler && feld[x+2][y+2] == Spieler && feld[x+3][y+3] == Spieler) {
-                ret = true;
-            }
-        }
 
-        if (x - 3 >= 0 && y + 3 < feld.length) {
-            if (feld[x][y] == Spieler && feld[x-1][y+1] == Spieler && feld[x-2][y+2] == Spieler && feld[x-3][y+3] == Spieler) {
-                ret = true;
-            }
-        }
-        return ret;
-    }
 }
--- src/main/ctrl/HelloApplication.java
+++ src/main/ctrl/HelloApplication.java
@@ -4,8 +4,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import main.Model.Spieler;
+import main.Model.Spielfeld;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class HelloApplication extends Application {
     @Override
@@ -19,5 +22,18 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
         launch();
+        Spielfeld spielfeld = new Spielfeld(1,2);
+
+        Spieler spieler1 = new Spieler("Zigo",2);
+        Spieler spieler2 = new Spieler("Sheeee",7);
+
+        Spieler prioSpieler = spieler1;
+
+        Scanner sc = new Scanner(System.in);
+        for (int i = 1; i <3;i++){
+            System.out.println(prioSpieler.getName() + ", Your turn\n");
+            spielfeld.Ausgabe();
+
+        }
     }
 }
